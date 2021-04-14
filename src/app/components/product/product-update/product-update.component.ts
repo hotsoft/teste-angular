4dba71936e77edd4bb3e8cab64ebea1c9fea7464
Roger F. Del Nobre <desenv4@labplus.com.br>
2021-04-14 17:03:22 -0300

    Desenvolvendo componente update produto e ajuste de erro undefined type

--- src/app/components/product/product-update/product-update.component.ts
+++ src/app/components/product/product-update/product-update.component.ts
@@ -1,15 +1,34 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  styleUrls: ['./product-update.component.css'],
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id as string).subscribe(product => {
+      this.product = product
+    });
   }
 
+  updateProduct(): void {}
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
